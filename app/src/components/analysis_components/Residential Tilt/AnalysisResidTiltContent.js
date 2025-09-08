a394c473058aacfc3fc9a47081c3875ac9674367
Jared <Jgib5432@gmail.com>
2025-09-08 17:32:00 -0600

    daily work 9-8-2025

---
+++ app/src/components/analysis_components/Residential Tilt/AnalysisResidTiltContent.js
@@ -0,0 +1,12 @@
+"use client";
+
+export function AnalysisResidTiltContent() {
+  return (
+    <div className="flex flex-col w-full p-4 items-center gap-8 text-center text-black">
+      <div className="text-6xl italic mt-8">Residential Tilt:</div>
+      <div className="text-4xl italic">
+        Why taxes are going up on homeowners
+      </div>
+    </div>
+  );
+}
